4993c4e17e77cc838cefc7fa19cb90bf575e69c6
Evan Hsu <evan_hsu@intuit.com>
2020-09-23 22:28:14 -0600

    Logo image is now square

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,30 @@
 import React from "react";
-import logo from "../images/tally-logo-138x182.png";
+import logo from "../images/tally-logo-120x120.png";
 
+type Size = "small" | "medium" | "large";
 export type LogoProps = {
   width?: number;
   height?: number;
+  size?: Size;
 };
 
+const sizes = {
+  small: {
+    width: 30,
+    height: 30,
+  },
+  medium: {
+    width: 50,
+    height: 50,
+  },
+  large: {
+    width: 120,
+    height: 120,
+  },
+};
 export const Logo = (props: LogoProps) => {
-  const { width = 130, height = 150 } = props;
+  const { width = 130, height = 150 } = props.size
+    ? sizes[props.size]
+    : { width: props.width, height: props.height };
   return <img src={logo} style={{ width: width, height: height }} />;
 };
